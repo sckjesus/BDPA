a86462d5b38a37a69dc0ab7f33c40fc4a3e09fd1
sckjesus <47199108+sckjesus@users.noreply.github.com>
2022-05-01 13:41:15 -0500

    Add files via upload

---
+++ hello-world.php
@@ -0,0 +1,8 @@
+<?php
+
+echo '<html>';
+echo '<body>';
+echo 'Hello world';
+echo '</body>';
+echo '</html>';
+?>
---
+++ name_age_school.php
@@ -0,0 +1,17 @@
+<?php
+
+echo '<html>';
+echo '<head>';
+echo '</head>';
+echo '<body>';
+echo '<p>';
+echo 'My name is Samuel C. Kaspar. ';
+echo 'I am 12 years old, and I go to ';
+echo 'John Adams Middle School';
+echo '</p>';
+echo '<a href="hello-world.php">';
+echo 'Hello World';
+echo '</a>';
+echo '</body>';
+echo '</html>';
+?>
